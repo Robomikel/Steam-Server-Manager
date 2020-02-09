1f82af102cf89f94986b1fa664e8de2075dda3c6
Robomikel <michaelawilliams28@outlook.com>
2020-02-09 01:48:05 -0700

    intial

--- functions/command_validate.ps1
+++ functions/command_validate.ps1
@@ -1,6 +1,11 @@
 Function Get-ValidateServer {
     Set-Location $global:currentdir\SteamCMD\ >$null 2>&1
     #Get-Steamtxt
+    Write-Host '****   Validate May Overwrite some config files   ****' -F R -B Black
+    Write-Host '****   Run Install command again to update variables-$global:server.ps1  ****' -F Y -B Black
+    Write-Host -NoNewLine 'Press any key to continue...';
+    $null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
+
     Write-Host '****   Validating Server   ****' -F M -B Black
     #.\steamcmd +runscript Validate-$global:server.txt
     If ($global:ANON -eq "yes") {
