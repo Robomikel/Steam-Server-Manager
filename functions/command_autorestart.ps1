5e18eb82163ed72512fbe62f0b4d3bbf47b9792b
Robomikel <michaelawilliams28@outlook.com>
2020-02-09 21:00:15 -0700

    Intial

--- functions/command_autorestart.ps1
+++ functions/command_autorestart.ps1
@@ -5,15 +5,27 @@ Function New-RestartJobBG {
     $restartTime = Read-Host
     Write-Host "Username: $env:COMPUTERNAME\$env:UserName"
     $SecurePassword = $password = Read-Host "Password:" -AsSecureString
-    $CRentials = New-Object System.Management.Automation.PSCRential -ArgumentList $UserName, $SecurePassword
-    $Password = $CRentials.GetNetworkCRential().Password  
+    $Credentials = New-Object System.Management.Automation.PSCredential -ArgumentList $UserName, $SecurePassword
+    $Password = $Credentials.GetNetworkCredential().Password  
     $Action = New-ScheduledTaskAction -Execute 'powershell.exe' -Argument "$global:currentdir\steamer.ps1 restart $global:server" -WorkingDirectory "$global:currentdir"
     #$Trigger = New-ScheduledTaskTrigger -Once -At (Get-Date).Date -RepetitionInterval (New-TimeSpan -Minutes $restartTime)
     $Trigger = New-ScheduledTaskTrigger -Daily -At $restartTime
     $Settings = New-ScheduledTaskSettingsSet -ExecutionTimeLimit '00:00:00'
     $Task = New-ScheduledTask -Action $Action -Trigger $Trigger -Settings $Settings
     Write-Host "Creating Task........" -F M -B Black
-    Register-ScheduledTask -TaskName "$global:server AutoRestart" -InputObject $Task -User "$UserName" -Password "$Password"
+    Register-ScheduledTask -TaskName "$global:server AutoRestart" -InputObject $Task -User "$UserName" -Password "$Password" -ea SilentlyContinue
+    If (!$?){
+        Write-Host " ****   Creating Scheduled Task Failed   ****" -F R -B Black 
+        Write-Host " ****   TRY this
+        1. (with admin account)Go to the Start menu
+        2. Run
+        3. Type secpol.msc and press Enter
+        4. The Local Security Policy manager opens
+        5. Go to Security Settings – Local Policies – User Rights Assignment node
+        6. Double click Log on as a batch job on the right side
+        7. Click Add User or Group…
+        8. Select the user and click OK  ****" -F Y -B Black 
+    }
 }
 Function New-RestartJob {
     Write-Host "Run Task only when user is logged on"
--- functions/command_monitor.ps1
+++ functions/command_monitor.ps1
@@ -13,12 +13,24 @@ Function New-MontiorJobBG {
     Write-Host "Run Task Whether user is logged on or not"
     Write-Host "Username: $env:COMPUTERNAME\$env:UserName"
     $SecurePassword = $password = Read-Host "Password:" -AsSecureString
-    $CRentials = New-Object System.Management.Automation.PSCRential -ArgumentList $UserName, $SecurePassword
-    $Password = $CRentials.GetNetworkCRential().Password 
+    $Credentials = New-Object System.Management.Automation.PSCredential -ArgumentList $UserName, $SecurePassword
+    $Password = $Credentials.GetNetworkCredential().Password 
     $Action = New-ScheduledTaskAction -Execute 'powershell.exe' -Argument "`"If (!(Get-Process '$global:PROCESS')) {$global:currentdir\steamer.ps1 start $global:server ;; $global:currentdir\steamer.ps1 discord $global:server }`"" -WorkingDirectory "$global:currentdir"
     $Trigger = New-ScheduledTaskTrigger -Once -At (Get-Date).Date -RepetitionInterval (New-TimeSpan -Minutes 5) 
     $Settings = New-ScheduledTaskSettingsSet -ExecutionTimeLimit '00:00:00'
     $Task = New-ScheduledTask -Action $Action -Trigger $Trigger -Settings $Settings
     Write-Host "Creating Task........" -F M -B Black
     Register-ScheduledTask -TaskName "$global:server monitor" -InputObject $Task -User "$UserName" -Password "$Password"
+    If (!$?){
+        Write-Host " ****   Creating Scheduled Task Failed   ****" -F R -B Black 
+        Write-Host " ****   TRY this
+        1. (with admin account)Go to the Start menu
+        2. Run
+        3. Type secpol.msc and press Enter
+        4. The Local Security Policy manager opens
+        5. Go to Security Settings – Local Policies – User Rights Assignment node
+        6. Double click Log on as a batch job on the right side
+        7. Click Add User or Group…
+        8. Select the user and click OK  ****" -F Y -B Black 
+    }
 }
