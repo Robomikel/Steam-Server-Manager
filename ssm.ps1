c0bd7c9c5739414f6e838fc0d358e2bf3e0b10c4
Robomikel <michaelawilliams28@outlook.com>
2020-02-15 01:47:19 -0700

    updated ifconfig.me webrequest

--- ssm.ps1
+++ ssm.ps1
@@ -10,7 +10,7 @@ $global:command = $($args[0])
 $global:server = $($args[1])
 $global:currentdir = Get-Location
 $global:serverdir = "$global:currentdir\$global:server"
-${global:EXTIP} = (Invoke-WebRequest -uri "http://ifconfig.me/ip" -ea SilentlyContinue ).Content
+${global:EXTIP} = (Invoke-WebRequest -uri "http://ifconfig.me/ip"  -UseBasicParsing -ea SilentlyContinue ).Content
 ${global:IP} = ((ipconfig | findstr [0-9].\.)[0]).Split()[-1]
 $global:Date = get-date -Format yyyyMMddTHHmmssffff
 # Game-Server-configs
