c982ef4c44eafedcf66181e74ca33a515794352e
Robomikel <michaelawilliams28@outlook.com>
2020-02-18 17:15:31 -0700

    Updated

--- functions/install_ssmupdate.ps1
+++ functions/install_ssmupdate.ps1
@@ -11,21 +11,22 @@ Function Get-UpdateSteamer {
     Write-Host '****   Downloading Steam-Server-Manager github files   ****' -F M -B Black 
     #(New-Object Net.WebClient).DownloadFile("$global:steamerurl", "$global:currentdir\steamer.zip")
     [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12;
-    Invoke-WebRequest -Uri $global:steamerurl -OutFile $global:currentdir\Steam-Server-Manager.zip
+    Invoke-WebRequest -Uri $global:steamerurl -OutFile Steam-Server-Manager.zip
     Write-Host "Download Time:  $((Get-Date).Subtract($start_time).Seconds) second(s)" -F Y -B Black 
-    Remove-Item  "$global:currentdir\Steam-Server-Manager\*" -Recurse -Force -ea SilentlyContinue
-    Remove-Item  "$global:currentdir\config-default\*" -Recurse -Force -ea SilentlyContinue
-    Remove-Item  "$global:currentdir\data\*" -Recurse -Force -ea SilentlyContinue
-    Remove-Item  "$global:currentdir\functions\*" -Recurse -Force -ea SilentlyContinue
-    Remove-Item  "$global:currentdir\README.md*" -Recurse -Force -ea SilentlyContinue
-    Remove-Item  "$global:currentdir\LICENSE*" -Recurse -Force -ea SilentlyContinue
-    Expand-Archive "$global:currentdir\Steam-Server-Manager.zip" "$global:currentdir\Steam-Server-Manager" -Force
-    If ($?){Copy-Item  "$global:currentdir\Steam-Server-Manager\Steam-Server-Manager-master\*" -Destination "$global:currentdir\" -Recurse -Force
+    Remove-Item  "Steam-Server-Manager\*" -Recurse -Force -ea SilentlyContinue
+    Remove-Item  "config-default\*" -Recurse -Force -ea SilentlyContinue
+    Remove-Item  "data\*" -Recurse -Force -ea SilentlyContinue
+    Remove-Item  "functions\*" -Recurse -Force -ea SilentlyContinue
+    Remove-Item  "README.md*" -Recurse -Force -ea SilentlyContinue
+    Remove-Item  "LICENSE*" -Recurse -Force -ea SilentlyContinue
+    Expand-Archive Steam-Server-Manager.zip Steam-Server-Manager -Force
     Start-Sleep 3
-    Remove-Item  "$global:currentdir\Steam-Server-Manager" -Recurse -Force -ea SilentlyContinue
-    Remove-Item  "$global:currentdir\Steam-Server-Manager.zip" -Recurse -Force -ea SilentlyContinue
+    Copy-Item  "$global:currentdir\Steam-Server-Manager\Steam-Server-Manager-master\*" -Destination "$global:currentdir\" -Recurse -Force
+    Start-Sleep 3
+    Remove-Item  "Steam-Server-Manager" -Recurse -Force -ea SilentlyContinue
+    Remove-Item  "Steam-Server-Manager.zip" -Recurse -Force -ea SilentlyContinue
     Write-Host '****   ssm github files Updated   ****' -F Y -B Black
     Write-Host '****   Press Enter to Close this session   ****' -F Y -B Black
     Pause  
-    Stop-Process -Id $PID}
+    Stop-Process -Id $PID
 }
