6c4b2f043ded023cf90a073b1d2e1603d3417ab7
Robomikel <michaelawilliams28@outlook.com>
2020-02-24 22:20:34 -0700

    Updated

--- functions/command_install.ps1
+++ functions/command_install.ps1
@@ -7,7 +7,7 @@
 #  "YMmMY"     MMM     """"YUMMMYMM   ""` MMM  M'  "MMM "YMmMY" MMMM   "W"   MP       MMM  M'  "MMM  `'YMUP"YMMMMMM   "W" 
 #
 #
-Function Install-ServerFiles {
+Function Install-ServerFiles_old {  
 
     Set-Location $steamdirectory
     If ($ANON -eq "yes") {
