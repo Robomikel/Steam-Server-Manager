a0dff6da97aeb175dfc745edbb862c27264ddb85
Robomikel <michaelawilliams28@outlook.com>
2020-03-01 13:09:51 -0700

    Streamline - Update

--- config-default/SniperElite4_default.ps1
+++ config-default/SniperElite4_default.ps1
@@ -10,7 +10,7 @@ Function  New-LaunchScriptSE4serverPS {
     # Server.AuthPort 8701
     # Server.GamePort 24001
     # Server.UpdatePort 23001
-     #Server.LobbyPort 25001
+    #Server.LobbyPort 25001
     
     
     ###################### Do not change below #####################
@@ -33,7 +33,36 @@ Function  New-LaunchScriptSE4serverPS {
     #                           Game-Server-Config
     $global:servercfg           = "server.cfg"
     #                           Server Launch Command
-    $global:launchParams        = '@("$executable exec $servercfgdir\Server.cfg")'
+    $global:launchParams        = '@("$executable exec $servercfgdir\Server.cfg -logdir=$serverdir")'
     # Get User Input version must be set to 0
     Get-UserInput
+    # Install Adjustment
+    New-servercfgse4
+}
+
+Function New-servercfgse4 {
+    New-Item $servercfgdir\Server.cfg -ItemType File -Force
+    Add-Content $servercfgdir\Server.cfg `
+"
+Server.Name Team Deathmatch - Map voting / Hardcore
+Server.MoTD Welcome to Sniper Elite 4
+
+Settings.MaxPlayers 12
+Settings.MaxLatency 999
+
+Settings.DefaultScoreLimit 0
+Settings.DefaultTimeLimit 15
+
+Lobby.StartTimer 90
+
+// Allow players to vote on the next map...
+Lobby.AutoTriggerMapVote 1
+// ... but we still need to specify the initial map with the correct game mode
+MapRotation.AddMap VILLAGE TDM
+// Add the game settings
+exec Preset_Hardcore.cfg
+
+Server.GamePort 27102
+Server.Host
+"
 }
