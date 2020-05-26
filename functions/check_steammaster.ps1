8e1dabe730435340082cd809d9548d29a63d1f00
Robomikel <michaelawilliams28@outlook.com>
2020-05-25 18:30:27 -0600

    discard last change

--- functions/check_steammaster.ps1
+++ functions/check_steammaster.ps1
@@ -5,9 +5,6 @@ Function Test-SteamMaster {
 		if ($Useprivate = "off") { 
 			if ( ${extip} -and ${queryport}) { 
 				if ( ${steammastercheck} -eq "on" ) {
-					If ($discorddisplayip) { 
-						${extip} = 	$discorddisplayip
-					}
 					$global:masterserver = Invoke-Webrequest "https://api.steampowered.com/ISteamApps/GetServersAtAddress/v0001?addr=${extip}:${queryport}" -UseBasicParsing | ConvertFrom-Json | Select-Object -ExpandProperty response | Select-Object -ExpandProperty Servers
 					Add-Content $ssmlog "[$loggingdate] https://api.steampowered.com/ISteamApps/GetServersAtAddress/v0001?addr=${extip}:${queryport} "
 					# $masterserver = $masterserver.response.success
@@ -17,9 +14,6 @@ Function Test-SteamMaster {
 			}
 			Elseif (${extip} -and ${port} ) { 
 				if ( ${steammastercheck} -eq "on" ) {
-					If ($discorddisplayip) { 
-						${extip} = 	$discorddisplayip
-					} 
 					$global:masterserver = Invoke-Webrequest "https://api.steampowered.com/ISteamApps/GetServersAtAddress/v0001?addr=${extip}:${port}" -UseBasicParsing | ConvertFrom-Json | Select-Object -ExpandProperty response | Select-Object -ExpandProperty Servers
 					Add-Content $ssmlog "[$loggingdate] https://api.steampowered.com/ISteamApps/GetServersAtAddress/v0001?addr=${extip}:${port} "
 					# $masterserver = $masterserver.response.success
