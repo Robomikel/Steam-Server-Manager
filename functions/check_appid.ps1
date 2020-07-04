10385046c2aa83e681a90a5650ebcb358cc97bc5
michaelwilliams <michaelawilliams28@gmail.com>
2020-07-04 09:37:01 -0600

    Updated - Read Appid -> switch

--- functions/check_appid.ps1
+++ functions/check_appid.ps1
@@ -8,225 +8,75 @@
 #
 Function Read-AppID {
     Write-log "Function: Read-AppID"
-    If ($AppID ) {
-        # Set-Console  >$null 2>&1
-        If ($AppID -eq 302200) {
-            New-LaunchScriptMiscreatedPS
-        }
-        ElseIf ($AppID -eq 294420) {
-            New-LaunchScriptSdtdserverPS
-        }
-        ElseIf ($AppID -eq 237410) {
-            New-LaunchScriptInsserverPS
-        }
-        ElseIf ($AppID -eq 581330) {
-            New-LaunchScriptInssserverPS
-        }
-        ElseIf ($AppID -eq 233780) {
-            New-LaunchScriptArma3serverPS
-        }
-        ElseIf ($AppID -eq 258550) {
-            New-LaunchScriptRustPS
-        }
-        ElseIf ($AppID -eq 376030) {
-            New-LaunchScriptArkPS
-        }
-        ElseIf ($AppID -eq 462310) {
-            New-LaunchScriptdoiserverPS
-        }
-        ElseIf ($AppID -eq 740) {
-            New-LaunchScriptcsgoserverPS
-        }
-        ElseIf ($AppID -eq 530870) {
-            New-LaunchScriptempserverPS
-        }
-        ElseIf ($AppID -eq 443030) {
-            New-LaunchScriptceserverPS
-        }
-        ElseIf ($AppID -eq 565060) {
-            New-LaunchScriptavserverPS
-        }
-        ElseIf ($AppID -eq 232130) {
-            New-LaunchScriptKF2serverPS
-        }
-        ElseIf ($AppID -eq 222860) {
-            New-LaunchScriptLFD2serverPS
-        }
-        ElseIf ($AppID -eq 454070) {
-            New-LaunchScriptboundElserverPS
-        }
-        ElseIf ($AppID -eq 556450) {
-            New-LaunchScriptforestserverPS
-        }
-        ElseIf ($AppID -eq 17515) {
-            New-LaunchScriptAoCserverPS
-        }
-        ElseIf ($AppID -eq 302550) {
-            New-LaunchScriptacserverPS
-        }
-        ElseIf ($AppID -eq 582400) {
-            New-LaunchScriptasrdserverPS
-        }
-        ElseIf ($AppID -eq 416880) {
-            New-LaunchScriptBOserverPS
-        }
-        ElseIf ($AppID -eq 985050) {
-            New-LaunchScriptAHL2serverPS
-        }
-        ElseIf ($AppID -eq 475370) {
-            New-LaunchScriptBB2serverPS
-        }
-        ElseIf ($AppID -eq 232370) {
-            New-LaunchScriptHL2DMserverPS
-        }
-        ElseIf ($AppID -eq 17585) {
-            New-LaunchScriptDystopiaserverPS
-        }
-        ElseIf ($AppID -eq 346680) {
-            New-LaunchScriptBlackMesaserverPS
-        }
-        ElseIf ($AppID -eq 232290) {
-            New-LaunchScriptDODSserverPS
-        }
-        ElseIf ($AppID -eq 343050) {
-            New-LaunchScriptDSTserverPS
-        }
-        ElseIf ($AppID -eq 4020) {
-            New-LaunchScriptGMODserverPS
-        }
-        ElseIf ($AppID -eq 232250) {
-            New-LaunchScriptTF2serverPS
-        }
-        ElseIf ($AppID -eq 317670) {
-            New-LaunchScriptNMRIHserverPS
-        }
-        ElseIf ($AppID -eq 228780) {
-            New-LaunchScriptbsserverPS
-        }
-        ElseIf ($AppID -eq 295230) {
-            New-LaunchScriptFOFserverPS
-        }
-        ElseIf ($AppID -eq 380870) {
-            New-LaunchScriptpzserverPS
-        }
-        ElseIf ($AppID -eq 276060) {
-            New-LaunchScriptSvenCoopserverPS
-        }
-        ElseIf ($AppID -eq 805140) {
-            New-LaunchScriptBT1944serverPS
-        }
-        ElseIf ($AppID -eq 1026340) {
-            New-LaunchScriptBTserverPS
-        }
-        ElseIf ($AppID -eq 383410) {
-            New-LaunchScriptCCserverPS
-        }
-        ElseIf ($AppID -eq 261140) {
-            New-LaunchScriptjc2serverPS
-        }
-        ElseIf ($AppID -eq 619960) {
-            New-LaunchScriptjc3serverPS
-        }
-        ElseIf ($AppID -eq 4940) {
-            New-LaunchScriptNS2serverPS
-        }
-        ElseIf ($AppID -eq 332670) {
-            New-LaunchScriptPCserverPS
-        }
-        ElseIf ($AppID -eq 17505) {
-            New-LaunchScriptZPSserverPS
-        }
-        ElseIf ($AppID -eq 403240) {
-            New-LaunchScriptSquadserverPS
-        }
-        ElseIf ($AppID -eq 1088320) {
-            New-LaunchScriptDofDserverPS
-        }
-        ElseIf ($AppID -eq 541790) {
-            New-LaunchScriptdowserverPS
-        }
-        ElseIf ($AppID -eq 568880) {
-            New-LaunchScriptSE4serverPS
-        }
-        ElseIf ($AppID -eq 298740) {
-            New-LaunchScriptSEserverPS
-        }
-        ElseIf ($AppID -eq 824360) {
-            New-LaunchScriptPixArkPS
-        }
-        ElseIf ($AppID -eq 667230) {
-            New-LaunchScriptCaspaServerPS
-        }
-        ElseIf ($AppID -eq 460040) {
-            New-LaunchScriptemserverPS
-        }
-        ElseIf ($AppID -eq 42750) {
-            New-LaunchScriptCOD3MWserverPS
-        }
-        ElseIf ($AppID -eq 11421000) {
-            New-LaunchScriptbedrock_serverPS
-        }
-        ElseIf ($AppID -eq 11500000) {
-            New-LaunchScriptminecraft_serverPS
-        }
-        ElseIf ($AppID -eq 600760) {
-            New-LaunchScriptStationeersPS
-        }
-        ElseIf ($AppID -eq 400300) {
-            New-LaunchScriptrFactor2serverPS
-        }
-        ElseIf ($AppID -eq 1017070) {
-            New-LaunchScriptVanguard1944serverPS
-        }
-        ElseIf ($AppID -eq 996560) {
-            New-LaunchScriptSCPSecretLaboratoryserverPS
-        }
-        ElseIf ($AppID -eq 770340) {
-            New-LaunchScriptIronArmadaserverPS
-        }
-        ElseIf ($AppID -eq 367970) {
-            New-LaunchScriptMEserverPS
-        }
-        ElseIf ($AppID -eq 1247090) {
-            New-LaunchScriptStormworksserverPS 
-        }
-        ElseIf ($AppID -eq 897590) {
-            New-LaunchScriptMemoriesofMarsServerPS 
-        }
-        ElseIf ($AppID -eq 1110390) {
-            New-LaunchScriptunturnedserverPS 
-        }
-        ElseIf ($AppID -eq 299310) {
-            New-LaunchScriptSeriousSamserverPS 
-        }
-        ElseIf ($AppID -eq 748090) {
-            New-LaunchScriptColonySurvivalServerPS 
-        }
-        ElseIf ($AppID -eq 1222650) {
-            New-LaunchScriptSpacecImpossibleserverPS 
-        }
-        ElseIf ($AppID -eq 336400) {
-            New-LaunchScriptPrimalCarnagePS 
-        }
-        ElseIf ($AppID -eq 423410) {
-            New-LaunchScriptBlackwakePS
-        }
-        ElseIf ($AppID -eq 238430) {
-            New-LaunchScriptContagionDSPS
-        }
-        #    ElseIf ($AppID -eq Template) {
-        #
-        #        New-LaunchScriptTemplateserverPS
-        #    } 336400
-        Else {
-            Get-warnmessage "nolaunchscript"
-            
-        }
-        If(!$?){
-            Get-warnmessage "readappidfailed"
-        }
-    }
-    Else {
-        Get-warnmessage "readappidfailed"
+    switch ($AppID) {
+        302200 { New-LaunchScriptMiscreatedPS ; Break }
+        294420 { New-LaunchScriptSdtdserverPS ; Break }
+        237410 { New-LaunchScriptInsserverPS ; Break }
+        581330 { New-LaunchScriptInssserverPS ; Break }
+        233780 { New-LaunchScriptArma3serverPS ; Break }
+        258550 { New-LaunchScriptRustPS ; Break }
+        376030 { New-LaunchScriptArkPS ; Break }
+        462310 { New-LaunchScriptdoiserverPS ; Break }
+        740 { New-LaunchScriptcsgoserverPS ; Break }
+        530870 { New-LaunchScriptempserverPS ; Break }
+        443030 { New-LaunchScriptceserverPS ; Break }
+        565060 { New-LaunchScriptavserverPS ; Break }
+        232130 { New-LaunchScriptKF2serverPS ; Break }
+        222860 { New-LaunchScriptLFD2serverPS ; Break }
+        454070 { New-LaunchScriptboundElserverPS ; Break }
+        556450 { New-LaunchScriptforestserverPS ; Break }
+        17515 { New-LaunchScriptAoCserverPS ; Break }
+        302550 { New-LaunchScriptacserverPS ; Break }
+        582400 { New-LaunchScriptasrdserverPS ; Break }
+        416880 { New-LaunchScriptBOserverPS ; Break }
+        985050 { New-LaunchScriptAHL2serverPS ; Break }
+        475370 { New-LaunchScriptBB2serverPS ; Break }
+        232370 { New-LaunchScriptHL2DMserverPS ; Break }
+        17585 { New-LaunchScriptDystopiaserverPS ; Break }
+        346680 { New-LaunchScriptBlackMesaserverPS ; Break }
+        232290 { New-LaunchScriptDODSserverPS ; Break }
+        343050 { New-LaunchScriptDSTserverPS ; Break }
+        4020 { New-LaunchScriptGMODserverPS ; Break }
+        232250 { New-LaunchScriptTF2serverPS ; Break }
+        317670 { New-LaunchScriptNMRIHserverPS ; Break }
+        228780 { New-LaunchScriptbsserverPS ; Break }
+        295230 { New-LaunchScriptFOFserverPS ; Break }
+        380870 { New-LaunchScriptpzserverPS ; Break }
+        276060 { New-LaunchScriptSvenCoopserverPS ; Break }
+        805140 { New-LaunchScriptBT1944serverPS ; Break }
+        1026340 { New-LaunchScriptBTserverPS ; Break }
+        383410 { New-LaunchScriptCCserverPS ; Break }
+        261140 { New-LaunchScriptjc2serverPS ; Break }
+        619960 { New-LaunchScriptjc3serverPS ; Break }
+        4940 { New-LaunchScriptNS2serverPS ; Break }
+        332670 { New-LaunchScriptPCserverPS ; Break }
+        17505 { New-LaunchScriptZPSserverPS ; Break }
+        403240 { New-LaunchScriptSquadserverPS ; Break }
+        1088320 { New-LaunchScriptDofDserverPS ; Break }
+        541790 { New-LaunchScriptdowserverPS ; Break }
+        568880 { New-LaunchScriptSE4serverPS ; Break }
+        298740 { New-LaunchScriptSEserverPS ; Break }
+        824360 { New-LaunchScriptPixArkPS ; Break }
+        667230 { New-LaunchScriptCaspaServerPS ; Break }
+        460040 { New-LaunchScriptemserverPS ; Break }
+        42750 { New-LaunchScriptCOD3MWserverPS ; Break }
+        11421000 { New-LaunchScriptbedrock_serverPS ; Break }
+        11500000 { New-LaunchScriptminecraft_serverPS ; Break }
+        600760 { New-LaunchScriptStationeersPS ; Break }
+        400300 { New-LaunchScriptrFactor2serverPS ; Break }
+        1017070 { New-LaunchScriptVanguard1944serverPS ; Break }
+        996560 { New-LaunchScriptSCPSecretLaboratoryserverPS ; Break }
+        770340 { New-LaunchScriptIronArmadaserverPS ; Break }
+        367970 { New-LaunchScriptMEserverPS ; Break }
+        1247090 { New-LaunchScriptStormworksserverPS ; Break }
+        897590 { New-LaunchScriptMemoriesofMarsServerPS ; Break }
+        1110390 { New-LaunchScriptunturnedserverPS ; Break }
+        299310 { New-LaunchScriptSeriousSamserverPS ; Break }
+        748090 { New-LaunchScriptColonySurvivalServerPS ; Break }
+        1222650 { New-LaunchScriptSpacecImpossibleserverPS ; Break }
+        336400 { New-LaunchScriptPrimalCarnagePS ; Break }
+        423410 { New-LaunchScriptBlackwakePS ; Break }
+        238430 { New-LaunchScriptContagionDSPS ; Break }
+        Default { Get-warnmessage "nolaunchscript" }
     }
 }
