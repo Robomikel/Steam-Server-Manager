6787adbd49525a22c88e9df5d31ebe647816a8a0
michaelwilliams <michaelawilliams28@gmail.com>
2020-07-05 17:48:15 -0600

    Updated - ssm update

--- functions/install_ssmupdate.ps1
+++ functions/install_ssmupdate.ps1
@@ -8,7 +8,8 @@
 #
 Function Get-UpdateSteamer {
     Get-UpdateSteamerCSV
-    $getlocalssm = $(Get-ChildItem $currentdir\functions\ -Force) + $(Get-ChildItem $currentdir\ssm.ps1) + $((Import-Csv $currentdir\data\serverlist.csv)."Default-Config")
+    Get-UpdateSteamerConfigDefault
+    $getlocalssm = $(Get-ChildItem $currentdir\functions\ -Force) + $(Get-ChildItem $currentdir\ssm.ps1)
     If ($getlocalssm) {
         ForEach ($getlocalssm in $getlocalssm ) {  
             $global:getlocalssmname = $getlocalssm.Name
@@ -41,7 +42,6 @@ Function Get-UpdateSteamer {
             }
         }
         Remove-Item "$currentdir\tmp" -Recurse -Force
-        # Get-UpdateSteamerConfigDefault
         Write-Information 'Press Enter to Close this session' -InformationAction Continue
         Pause  
         Stop-Process -Id $PID
@@ -70,6 +70,42 @@ Function Get-UpdateSteamerCSV {
                                     Get-Infomessage 'ssmupdates' 'update'
                                     New-Item  "$currentdir\data\$getlocalssmname" -Force >$null 2>&1
                                     Add-Content "$currentdir\data\$getlocalssmname" $githubvarcontent -InformationAction  SilentlyContinue
+                                } 
+                                Else {
+                                    Get-Infomessage 'nossmupdates' 
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        Remove-Item "$currentdir\tmp" -Recurse -Force
+    }
+}
+Function Get-UpdateSteamerConfigDefault {
+    $getlocalssm = @($((Import-Csv $currentdir\data\serverlist.csv)."Default-Config"))
+    If ($getlocalssm) {
+        ForEach ($getlocalssm in $getlocalssm ) {  
+            $global:getlocalssmname = $getlocalssm
+            If ($getlocalssmname) {
+                $githubvarcontent = Invoke-WebRequest "https://raw.githubusercontent.com/Robomikel/Steam-Server-Manager/master/config-default/$getlocalssmname" -UseBasicParsing
+                If ($githubvarcontent) {
+                    $githubvarcontent = ($githubvarcontent).Content
+                    If ($githubvarcontent) {
+                        If (Test-Path $currentdir\tmp) { } Else {
+                            New-Item  . -Name 'tmp' -ItemType Directory -InformationAction  SilentlyContinue | Out-File -Append -Encoding Default  $ssmlog
+                        }
+                        New-Item  "$currentdir\tmp\$getlocalssmname\" -Force >$null 2>&1
+                        Add-Content "$currentdir\tmp\$getlocalssmname" $githubvarcontent -InformationAction  SilentlyContinue
+                        $githubvarcontenttrim = Get-Content $currentdir\tmp\$getlocalssmname | Where-Object { $_ -notlike "" }
+                        If ($githubvarcontenttrim) {
+                            $ssmcontentlocaltrim = Get-Content $currentdir\config-default\$getlocalssmname | Where-Object { $_ -notlike "" }
+                            If ($ssmcontentlocaltrim ) {
+                                if (Compare-Object ($githubvarcontenttrim ) ($ssmcontentlocaltrim )) {
+                                    Get-Infomessage 'ssmupdates' 'update'
+                                    New-Item  "$currentdir\config-default\$getlocalssmname" -Force >$null 2>&1
+                                    Add-Content "$currentdir\config-default\$getlocalssmname" $githubvarcontent -InformationAction  SilentlyContinue
                                 } 
                                 Else {
                                     Get-Infomessage 'nossmupdates' 
