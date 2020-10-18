4caaca533789c77f04f5eb86a8f0e80f422a8687
Robomikel <michaelawilliams28@outlook.com>
2020-10-17 23:08:28 -0600

    updated - mc java check

--- functions/install_minecraft.ps1
+++ functions/install_minecraft.ps1
@@ -23,23 +23,30 @@ Function Get-MCBRBinaries {
 Function Get-MCjavaBinaries {
     Write-log "Function: Get-MCjavaBinaries"
     #################### MineCraft Java Install ################
-    Get-MCWebrequest 
-    $mcWebResponse = ((Invoke-WebRequest "https://www.minecraft.net/en-us/download/server" -UseBasicParsing ).Links | Where-Object { $_.href -like "https://launcher.mojang.com/v1/objects/*/server.jar" })
-    Get-Infomessage  "Downloading" 'Minecraft Java'
-    Invoke-WebRequest -uri $mcWebResponse.href -O server.jar 
-    # $mcWebResponse.outerText
-    # Expand-Archive "bedrock-server.zip" "bedrock-server" -Force -ea SilentlyContinue
-    Move-Item server.jar $serverfiles\ -Force -ea SilentlyContinue
-    New-Item $serverfiles\version.txt -Force | Out-File -Append -Encoding Default  $ssmlog
-    Add-Content $serverfiles\version.txt $mcvWebResponse -Force
-    Set-Location $serverdir
-    If (!(Test-Path eula.txt )) {
-        Start-Process CMD "/c start java -Xms1024M -Xmx1024M -jar server.jar nogui" -Wait
-        Start-Sleep 3
-        ((Get-Content -path eula.txt -Raw) -replace "false", "true") | Set-Content -Path eula.txt
+    java -version
+        if ($?) {
+            Get-MCWebrequest 
+            $mcWebResponse = ((Invoke-WebRequest "https://www.minecraft.net/en-us/download/server" -UseBasicParsing ).Links | Where-Object { $_.href -like "https://launcher.mojang.com/v1/objects/*/server.jar" })
+            Get-Infomessage  "Downloading" 'Minecraft Java'
+            Invoke-WebRequest -uri $mcWebResponse.href -O server.jar 
+            # $mcWebResponse.outerText
+            # Expand-Archive "bedrock-server.zip" "bedrock-server" -Force -ea SilentlyContinue
+            Move-Item server.jar $serverfiles\ -Force -ea SilentlyContinue
+            New-Item $serverfiles\version.txt -Force | Out-File -Append -Encoding Default  $ssmlog
+            Add-Content $serverfiles\version.txt $mcvWebResponse -Force
+            Set-Location $serverdir
+            If (!(Test-Path eula.txt )) {
+                Start-Process CMD "/c start java -Xms1024M -Xmx1024M -jar server.jar nogui" -Wait
+                Start-Sleep 3
+                ((Get-Content -path eula.txt -Raw) -replace "false", "true") | Set-Content -Path eula.txt
+            }
+            Set-Location $currentdir
+            # Add-Content $serverfiles\eula.txt 'eula=true' -Force
+            # Remove-Item bedrock-server -Recurse -Force -ea SilentlyContinue
+            ##############################################################
+        }
+    Else {
+        Write-log "Failed: Java not Installed"
+        Write-Warning "Java not Installed" -WarningAction Stop
     }
-    Set-Location $currentdir
-    # Add-Content $serverfiles\eula.txt 'eula=true' -Force
-    # Remove-Item bedrock-server -Recurse -Force -ea SilentlyContinue
-    ##############################################################
 }
