f1c6d4853980420b0c9ffa4f5262e7a3331fd285
Robomikel <michaelawilliams28@outlook.com>
2021-02-26 23:18:17 -0700

    updated - menu performance

--- functions/core_commands.ps1
+++ functions/core_commands.ps1
@@ -32,11 +32,11 @@ Function Select-Steamer {
         'update' { Read-Param; Get-FolderNames; Get-createdvaribles; Get-CheckForVars; Get-ChecktaskDisable; Get-ServerBuildCheck; Get-ChecktaskEnable; Get-Finished; Test-VariablesNull; Get-ClearVariables; Break }
         'force-update' { Read-Param; Get-FolderNames; Get-createdvaribles; Get-CheckForVars; Get-CheckNonSteam; Get-ChecktaskDisable; Get-UpdateServer; Get-ChecktaskEnable; Get-Finished; Test-VariablesNull; Get-ClearVariables; Break }
         'validate' { Read-Param; Get-FolderNames; Get-createdvaribles; Get-CheckForVars; Get-CheckNonSteam; Get-ChecktaskDisable; Get-StopServer; Get-Steam; Get-ValidateServer; Get-ChecktaskEnable; Get-Finished; Test-VariablesNull; Get-ClearVariables; Break }
-        'start' { Read-Param; Get-FolderNames; Get-createdvaribles; Get-CheckForVars; Get-CheckServer; Edit-ServerConfig; Get-ServerBuildCheck; Select-StartServer; Get-ChecktaskEnable;Get-ChecktaskEnabler; Test-VariablesNull; Get-ClearVariables; Break }
-        'stop' { Read-Param; Get-FolderNames; Get-createdvaribles; Get-CheckForVars; Get-ChecktaskDisable;Get-ChecktaskDisabler; Get-StopServer; Test-VariablesNull; Get-ClearVariables; Break }
+        'start' { Read-Param; Get-FolderNames; Get-createdvaribles; Get-CheckForVars; Get-CheckServer; Edit-ServerConfig; Get-ServerBuildCheck; Select-StartServer; Get-ChecktaskEnable; Get-ChecktaskEnabler; Test-VariablesNull; Get-ClearVariables; Break }
+        'stop' { Read-Param; Get-FolderNames; Get-createdvaribles; Get-CheckForVars; Get-ChecktaskDisable; Get-ChecktaskDisabler; Get-StopServer; Test-VariablesNull; Get-ClearVariables; Break }
         'restart' { Read-Param; Get-FolderNames; Get-createdvaribles; Get-CheckForVars; Get-ChecktaskDisable; Get-ServerBuildCheck; Get-RestartsServer; Get-ChecktaskEnable; Test-VariablesNull; Get-ClearVariables; Break }
         'monitor' { Read-Param; Get-FolderNames; Get-createdvaribles; Get-CheckForVars; Get-GamedigServervMonitor; Get-MonitorServer; Test-VariablesNull; Get-ClearVariables; Break }
-        'backup' { Read-Param; Get-FolderNames; Get-createdvaribles; Get-CheckForVars; Get-SevenZipCheck; Get-ChecktaskDisable;Get-ChecktaskDisabler; New-BackupFolder  ; New-BackupServer; Get-ChecktaskEnable;Get-ChecktaskEnabler; Get-Finished; Test-VariablesNull; Get-ClearVariables; Break }
+        'backup' { Read-Param; Get-FolderNames; Get-createdvaribles; Get-CheckForVars; Get-SevenZipCheck; Get-ChecktaskDisable; Get-ChecktaskDisabler; New-BackupFolder  ; New-BackupServer; Get-ChecktaskEnable; Get-ChecktaskEnabler; Get-Finished; Test-VariablesNull; Get-ClearVariables; Break }
         'restore' { Read-Param; Get-FolderNames; Get-createdvaribles; Get-CheckForVars; Get-SevenZipCheck; Get-ChecktaskDisable; New-BackupFolder  ; Get-BackupMenu ; Get-ChecktaskEnable; Get-Finished; Test-VariablesNull; Get-ClearVariables; Break }
         'install-monitor' { Read-Param; Get-FolderNames; Get-createdvaribles; Get-CheckForVars; Set-MonitorJob; Test-VariablesNull; Get-ClearVariables; Break }
         'install-mod' { Read-Param; Get-FolderNames; Get-createdvaribles; Get-CheckForVars; Get-SevenZipCheck; Get-Modinstall; Test-VariablesNull; Get-ClearVariables; Break }
@@ -47,9 +47,9 @@ Function Select-Steamer {
         'discord' { Read-Param; Get-FolderNames; Get-createdvaribles; Get-CheckForVars; New-DiscordAlert; Test-VariablesNull; Get-ClearVariables; Break }
         'details' { Read-Param; Get-FolderNames; Get-createdvaribles ; Get-CheckForVars; Test-PSversion; Get-details; Test-VariablesNull; Get-ClearVariables; Break }
         'install-VcRedist' { Install-VisualCPlusPlus; Get-ClearVariables; Break }
-        'update-mods' { Read-Param;Get-FolderNames;Get-createdvaribles;Get-UpdateMods; Get-ClearVariables; Break }
+        'update-mods' { Read-Param; Get-FolderNames; Get-createdvaribles; Get-UpdateMods; Get-ClearVariables; Break }
         'stats' { Measure-stats; Break }
-        'menu' { Get-SSMMenu}
+        'menu' { Get-SSMMenu }
         'exit' { exit; Break }
         Default { Get-Help }
     }
@@ -59,12 +59,12 @@ Function Read-Param {
     Write-log "Function Read-Param"
     switch ($serverfiles) {
         { (!$serverfiles) } { 
-        Write-Host 'Input Server Folder Name: ' -F C -N
-         $s = Read-host
-         Set-Variable -Name serverfiles -Value $s -Scope Script -Force
-        Get-TestString
-        Set-SteamerSetting
-        Get-CustomSettings
+            Write-Host 'Input Server Folder Name: ' -F C -N
+            $s = Read-host
+            Set-Variable -Name serverfiles -Value $s -Scope Script -Force
+            Get-TestString
+            Set-SteamerSetting
+            Get-CustomSettings
         }
     }
 }
@@ -72,17 +72,16 @@ Function Get-SSMMenu {
     Write-log "Function: Get-SSMMenu"
     Write-Host ".:.:.:.:.:.:.:. SSM Command Menu .:.:.:.:.:.:.:.
     Choose Command: " -F Cyan
-    $command = Menu @('install A-E', 'install D-M', 'install N-Z', 'start', 'stop', 'update', 'restart', 'monitor', 'backup', 'restore', 'validate', 'install-monitor', 'install-mod', 'install-ws', 'force-update', 'install-Restart', 'query', 'mcrcon', 'discord', 'details', 'exit', 'stats','update-mods')
+    $command = Menu @('install A-E', 'install D-M', 'install N-Z', 'start', 'stop', 'update', 'restart', 'monitor', 'backup', 'restore', 'validate', 'install-monitor', 'install-mod', 'install-ws', 'force-update', 'install-Restart', 'query', 'mcrcon', 'discord', 'details', 'exit', 'stats', 'update-mods')
     clear-Host
     Set-Console  >$null 2>&1
     If ($command -ne "install A-E" -and $command -ne "install D-M" -and $command -ne "install N-Z") {
         Write-Host ".:.:.:.:.:.:.:. SSM Server Menu .:.:.:.:.:.:.:.
     Choose Server: 
     loading..." -F Cyan
-        $check = ((gci $sfwd -r |  ? name -like Variables-*.ps1 | select DirectoryName).DirectoryName).Replace("$sfwd\", "" )
+        $check = ((gci $sfwd -Depth 1 -Filter Variables-*.ps1).Directory.Name )
         if ($check.count -gt 1) {
-           $serverfiles = Menu (iex " ((gci $sfwd -r -depth 2 |  ? name -like Variables-*.ps1 | select DirectoryName).DirectoryName).Replace(`"$sfwd\`", `"`" )")
-           # $serverfiles = menu (iex "(gci * -Filter *server | select Name).Name")
+            $serverfiles = Menu (iex "(gci $sfwd -Depth 1 -Filter Variables-*.ps1).Directory.Name")
         }
         Elseif ($check.count -eq 1) {
             $serverfiles = $check
