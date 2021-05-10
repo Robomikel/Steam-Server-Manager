bd9b4ffdf5843b926fca20b1116345a2f0b01f1d
Robomikel <michaelawilliams28@outlook.com>
2021-05-09 18:37:09 -0600

    updated - storage output

--- functions/command_details.ps1
+++ functions/command_details.ps1
@@ -77,10 +77,10 @@ Function Get-Details {
     $totalfree = "{0:N2} GB" -f ($windows32.FreePhysicalMemory / 1MB)
     $totalmem = "{0:N2} GB" -f ($windows32.TotalVisibleMemorySize / 1MB)
     $totalusedmem = "{0:N2} GB" -f ( ( $windows32.TotalVisibleMemorySize - $windows32.FreePhysicalMemory) / 1MB)
-    $backups = ((Get-Childitem  $backupdir -recurse | Measure-Object).Count) 
-    $backupssize = "{0:N2} GB" -f ((Get-Childitem $backupdir | Measure-Object Length -Sum -ea silentlycontinue ).Sum / 1GB) 
-    $serverfilesdir = "{0:N2} GB" -f ((Get-Childitem $sfwd\$serverfiles | Measure-Object Length -Sum -ea silentlycontinue ).Sum / 1GB) 
-    $currentdir = "{0:N2} GB" -f ((Get-Childitem $currentdir  | Measure-Object Length -Sum -ea silentlycontinue ).Sum / 1GB) 
+    $backups = ((Get-Childitem -Depth 1 $backupdir | Measure-Object).Count) 
+    $backupssize = "{0:N2} GB" -f ((Get-Childitem -Depth 1 $backupdir | Measure-Object Length -Sum -ea silentlycontinue ).Sum / 1GB) 
+    $serverfilesdir = "{0:N2} GB" -f ((Get-Childitem $sfwd\$serverfiles -Recurse | Measure-Object Length -Sum -ea silentlycontinue ).Sum / 1GB) 
+    $currentdir = "{0:N2} GB" -f ((Get-Childitem $currentdir -Recurse | Measure-Object Length -Sum -ea silentlycontinue ).Sum / 1GB) 
     $directx = Get-ItemProperty "hklm:\Software\Microsoft\DirectX" 
     If ((Get-Process "$process" -ea SilentlyContinue)) {
         $gameservermem = "{0:N2} GB" -f ((Get-Process $process).WS / 1GB) 
