258fd4b9f679702e563fece895c162ac69d6a6ef
Robomikel <michaelawilliams28@outlook.com>
2025-11-22 00:31:58 -0700

    updated - ipconfig obsolete PS7

--- ssm.ps1
+++ ssm.ps1
@@ -22,7 +22,8 @@ If ($serverfiles) {
 # $serverfiles = $($args[1])
 $currentdir = $PSScriptRoot
 $ipv4 = '^((?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$'
-${IP} = ((ipconfig | findstr [0-9].\.)[0]).Split()[-1]
+${IP} = (Get-NetIPAddress).IPAddress -match '(10|172|192).\d{1,3}.\d{1,3}.\d{1,3}\b'
+#${IP} = ((ipconfig | findstr [0-9].\.)[0]).Split()[-1]
 $Date = get-date -Format yyyyMMddTHHmmssffff
 $loggingDate = get-date -Format MM-dd-yyyy-hh:mm:ss
 $errorloggingDate = get-date -Format MM-dd-yyyy-hhmm
